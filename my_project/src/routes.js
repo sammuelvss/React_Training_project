921ca9047b1d53c8beb4afc1ce3685c1ba59b74c
Sammuel Victor <samvss13@gmail.com>
2026-04-28 15:41:48 +0000

    1241

--- my_project/src/routes.js
+++ my_project/src/routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 
-function RouteApp() {
+function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
