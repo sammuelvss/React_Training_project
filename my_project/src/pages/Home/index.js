68f4b5e58114e5e4145d3bda1a8ea6166e49df7d
Sammuel Victor <samvss13@gmail.com>
2026-04-30 02:57:11 +0000

    2357

--- my_project/src/pages/Home/index.js
+++ my_project/src/pages/Home/index.js
@@ -1,23 +1,22 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-import { Link }   from 'react-router-dom';
-import './home.css';
+import { Link } from "react-router-dom";
+import "./home.css";
 
 // URL DA API: https://api.themoviedb.org/3/movie/popular?api_key=701882ba979a87e85e64cdd5d735f17f&language=pt-BR
 
 function Home() {
-
   const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
     async function loadFilmes() {
-      const response = await api.get('movie/popular', {
+      const response = await api.get("movie/popular", {
         params: {
-          api_key: '701882ba979a87e85e64cdd5d735f17f',
-          language: 'pt-BR',
+          api_key: "701882ba979a87e85e64cdd5d735f17f",
+          language: "pt-BR",
           page: 1,
-        }
-      })
+        },
+      });
 
       //console.log(response.data.results.slice(0, 10));
       setFilmes(response.data.results.slice(0, 10));
@@ -29,16 +28,20 @@ function Home() {
   return (
     <div className="container">
       <div className="lista-filmes">
+        <hr />
         {filmes.map((filme) => {
           return (
             <article key={filme.id}>
               <strong>{filme.title}</strong>
-              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
               <Link to={`/filme/${filme.id}`}>Acessar</Link>
-            </article>  
-          )   
+            </article>
+          );
         })}
-        </div>
+      </div>
     </div>
   );
 }
