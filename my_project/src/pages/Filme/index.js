4b8ccd6c2b0f3bac5a4cd4e4439782e35a69d1ca
Sammuel Victor <samvss13@gmail.com>
2026-04-30 21:19:10 +0000

    1819

--- my_project/src/pages/Filme/index.js
+++ my_project/src/pages/Filme/index.js
@@ -1,9 +1,60 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
+
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "701882ba979a87e85e64cdd5d735f17f",
+          language: "pt-BR",
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("Ops... Filme não encontrado!");
+      })
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("Componente desmontado");
+    }
+
+  }, []);
+
+  if(loading) {
+    return (
+      <div className="filme-info">
+        <h2>Carregando detalhes do filme...</h2>
+      </div>
+    );
+  }
+
+
   return (
     <div>
-      <h1>Bem vindo aos Filmes</h1>
+      <div className="filme-info">
+        <h1>{filme.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+        <p>{filme.overview}</p>
+        <p>{filme.vote_average}/10</p>
+      </div>
     </div>
   );
 }
-
+ 
 export default Filme;
--- my_project/src/pages/Home/index.js
+++ my_project/src/pages/Home/index.js
@@ -7,6 +7,8 @@ import "./home.css";
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     async function loadFilmes() {
@@ -20,18 +22,30 @@ function Home() {
 
       //console.log(response.data.results.slice(0, 10));
       setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
+
+  if(loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    );
+  }
+
   return (
+    
     <div className="container">
+      
       <div className="lista-filmes">
-        <hr />
         {filmes.map((filme) => {
           return (
             <article key={filme.id}>
+              <hr />
               <strong>{filme.title}</strong>
               <img
                 src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
